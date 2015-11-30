176546017b202071974236cb79211a05a3932930
LookMachine <lookmachine.co@gmail.com>
2015-11-30 02:12:08 -0800

    FInal Onboarding PT

---
+++ DrewsList/RMParallax-master copy/RMParallax/RMParallax/RMParallaxItem.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//RMParallax 
+//
+///  Created by Starflyer on 11/29/15.
+//  Copyright © 2015 abearablecode. All rights reserved.
+///
+
+import UIKit
+
+class RMParallaxItem {
+    
+    var image: UIImage!
+    var text: String!
+    
+    init(image: UIImage, text: String) {
+        self.image = image
+        self.text = text
+    }
+    
+}
