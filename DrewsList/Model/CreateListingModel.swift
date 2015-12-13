f45ad20c5a748f419d5fff9aef42e54d9abc5176
skewerb <skewerblack@gmail.com>
2015-12-13 14:02:53 -0800

    add contents of createListingModel

--- DrewsList/Model/CreateListingModel.swift
+++ DrewsList/Model/CreateListingModel.swift
@@ -7,3 +7,20 @@
 //
 
 import Foundation
+import Signals
+import ObjectMapper
+
+public class CreateListingModel : Mappable {
+    
+    public let _isbn = Signal<String?>()
+    public var isbn: String? { didSet { _isbn => isbn } }
+    
+    public let _book = Signal<Book?>()
+    public var book: Book? { didSet { _book => book } }
+    
+    required public init?(_ map: Map) {}
+    
+    public func mapping(map: Map) {
+        
+    }
+}
