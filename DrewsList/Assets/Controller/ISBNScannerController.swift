1fde6916966ef17816eae5b0cf727a8b5a90684d
Andrew Aquino <andrewaquino118@yahoo.com>
2015-12-20 14:22:17 -0800

    syxned project

---
+++ DrewsList/Assets/Controller/ISBNScannerController.swift
@@ -0,0 +1,9 @@
+//
+//  ISBNScannerController.swift
+//  DrewsList
+//
+//  Created by Andrew Aquino on 12/20/15.
+//  Copyright © 2015 Totem. All rights reserved.
+//
+
+import Foundation
---
+++ DrewsList/Assets/Model/ISBNScannerModel.swift
@@ -0,0 +1,9 @@
+//
+//  ISBNScannerModel.swift
+//  DrewsList
+//
+//  Created by Andrew Aquino on 12/20/15.
+//  Copyright © 2015 Totem. All rights reserved.
+//
+
+import Foundation
