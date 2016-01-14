4778427251ef23f61e041e142683953b401d668b
Andrew Aquino <andrewaquino118@yahoo.com>
2016-01-14 14:46:21 -0800

    scaffolded activity model, controller, and activity cell

--- DrewsList/Assets/Model/ActivityFeedModel.swift
+++ DrewsList/Assets/Model/ActivityFeedModel.swift
@@ -8,10 +8,36 @@
 
 import Foundation
 import Signals
+import RealmSwift
 
 public class ActivityFeedModel {
   
   public let _activity = Signal<String?>()
   public var activity: String? { didSet { _activity => activity } }
   
+  public let _activities = Signal<[Activity]>()
+  public var activities: [Activity] = [] { didSet { _activities => activities } }
+}
+
+public class Activity: Object {
+  
+  public let _activity = Signal<String?>()
+  public var activity: String? { didSet { _activity => activity } }
+  
+  public let _timestamp = Signal<String?>()
+  public var timestamp: String? { didSet { _timestamp => timestamp } }
+  
+  public let _leftImage = Signal<String?>()
+  public var leftImage: String? { didSet { _leftImage => leftImage } }
+
+  public let _rightImage = Signal<String?>()
+  public var rightImage: String? { didSet { _rightImage => rightImage } }
+  
+  public convenience init(activity: String?, timestamp: String?, leftImage: String?, rightImage: String?) {
+    self.init()
+    self.activity = activity
+    self.timestamp = timestamp
+    self.leftImage = leftImage
+    self.rightImage = rightImage
+  }
 }
