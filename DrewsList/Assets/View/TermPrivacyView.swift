93c8d04623207d977fde16b96d49c5b13c35a6d4
Vergil611 <erolnoble@gmail.com>
2016-02-28 11:33:05 -0800

    Terms and Privacy View
    Converted the Terms and Privacy View into DL-Table View

--- DrewsList/Assets/View/TermPrivacyView.swift
+++ DrewsList/Assets/View/TermPrivacyView.swift
@@ -77,7 +77,7 @@ class TermPrivacyView: UIViewController, UITableViewDelegate, UITextFieldDelegat
         cell.titleLabel?.text = "Notes"
         cell.inputTextView?.placeholder = "Drew's List Privacy Policy (updated January 1st, 2016).This policy details This policy details how data about you is used when you access our mobile app and services (together, DL) or interact with us. If we update it, we will revise the date, place notices on DL if changes are material, and/or obtain your consent as required by law. 1. Protecting your Privacy: We take precautions to prevent unauthorized access to or misuse of data about you.We do not employ tracking devices for marketing purposes.Please review privacy policies of any third party sites linked to from DL. 2. Data we use to provide/improve our services and/or combat fraud/abuse: Data you post on or send via DL, and/or send us directly or via other sites credit card data, which is transmitted to payment processors via a security protocol (e.g. SSL). data you submit or provide (e.g. name, address, email,phone, fax, photos, tax ID).data collected via cookies (e.g. search data and  favorites lists). data about your device(s) (e.g. screen size, DOM local storage, plugins).data from 3rd parties (e.g. phone type, geo-location via IP address).Data provided by Facebook integration (e.g. General Information, email). 3. Data we store: We retain data as needed for our business purposes and/or as required by law. We make good faith efforts to store data securely, but can make no guarantees. You may access and update certain data about you via your account login."
         
-      
+    
       
      return cell
       }
