dfd5a3a58e32466b64702233c44e20286615f921
Vergil611 <erolnoble@gmail.com>
2016-02-27 21:05:37 -0800

    Finish Term & Pri
    Need to finish Delete Button and Edit View for Server calls

--- DrewsList/Assets/View/TermPrivacyView.swift
+++ DrewsList/Assets/View/TermPrivacyView.swift
@@ -34,6 +34,7 @@ class TermPrivacyView: UIViewController, UITableViewDelegate, UITextFieldDelegat
     super.viewDidLoad()
     
     SetUpSelf()
+    //setupTableView()
     
     HeaderView?.anchorAndFillEdge(.Top, xPad: 0, yPad: 0, otherSize: 60)
     HeaderTitle?.anchorToEdge(.Bottom, padding: 12, width: 150, height: 24)
@@ -42,21 +43,51 @@ class TermPrivacyView: UIViewController, UITableViewDelegate, UITextFieldDelegat
     
     
     
+  
     myLabel.lineBreakMode = NSLineBreakMode.ByWordWrapping
     myLabel.numberOfLines = 0                      //'0' means infinite number of lines
     myLabel.textColor = UIColor.sexyGray()
     myLabel.text = "Drew's List Privacy Policy (updated January 1st, 2016).This policy details This policy details how data about you is used when you access our mobile app and services (together, DL) or interact with us. If we update it, we will revise the date, place notices on DL if changes are material, and/or obtain your consent as required by law. 1. Protecting your Privacy: We take precautions to prevent unauthorized access to or misuse of data about you.We do not employ tracking devices for marketing purposes.Please review privacy policies of any third party sites linked to from DL. 2. Data we use to provide/improve our services and/or combat fraud/abuse: Data you post on or send via DL, and/or send us directly or via other sites credit card data, which is transmitted to payment processors via a security protocol (e.g. SSL). data you submit or provide (e.g. name, address, email,phone, fax, photos, tax ID).data collected via cookies (e.g. search data and  favorites lists). data about your device(s) (e.g. screen size, DOM local storage, plugins).data from 3rd parties (e.g. phone type, geo-location via IP address).Data provided by Facebook integration (e.g. General Information, email). 3. Data we store: We retain data as needed for our business purposes and/or as required by law. We make good faith efforts to store data securely, but can make no guarantees. You may access and update certain data about you via your account login."
 
-
     self.view.addSubview(myLabel)
     
   }
   
-  
+
   private func SetUpSelf() {
     view.backgroundColor = .whiteColor()
     title = "Terms & Privacy"
    
   }
+  
+  private func setupTableView() {
+    TableView = DLTableView()
+    TableView?.delegate = self
+    view.addSubview(TableView!)
+  }
+  
+  
+  public func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return 1
+  }
+  public func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+    switch (indexPath.row) {
+    case 0:
+      if let cell = tableView.dequeueReusableCellWithIdentifier("InputTextViewCell", forIndexPath: indexPath) as? InputTextViewCell {
+        cell.titleLabel?.text = "Notes"
+        cell.inputTextView?.placeholder = "Drew's List Privacy Policy (updated January 1st, 2016).This policy details This policy details how data about you is used when you access our mobile app and services (together, DL) or interact with us. If we update it, we will revise the date, place notices on DL if changes are material, and/or obtain your consent as required by law. 1. Protecting your Privacy: We take precautions to prevent unauthorized access to or misuse of data about you.We do not employ tracking devices for marketing purposes.Please review privacy policies of any third party sites linked to from DL. 2. Data we use to provide/improve our services and/or combat fraud/abuse: Data you post on or send via DL, and/or send us directly or via other sites credit card data, which is transmitted to payment processors via a security protocol (e.g. SSL). data you submit or provide (e.g. name, address, email,phone, fax, photos, tax ID).data collected via cookies (e.g. search data and  favorites lists). data about your device(s) (e.g. screen size, DOM local storage, plugins).data from 3rd parties (e.g. phone type, geo-location via IP address).Data provided by Facebook integration (e.g. General Information, email). 3. Data we store: We retain data as needed for our business purposes and/or as required by law. We make good faith efforts to store data securely, but can make no guarantees. You may access and update certain data about you via your account login."
+        
+      
+      
+     return cell
+      }
+      break;
+    default:
+      break;
+    }
+    
+    return DLTableViewCell()
+    
+}
   
 }
