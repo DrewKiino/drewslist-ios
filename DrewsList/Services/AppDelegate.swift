5518eddfa3db15a57dd40155439eb0f52a53d03d
Andrew Aquino <andrewaquino118@yahoo.com>
2016-03-06 11:37:21 -0800

    added production server url

--- DrewsList/Services/AppDelegate.swift
+++ DrewsList/Services/AppDelegate.swift
@@ -22,13 +22,16 @@ public let screen = UIScreen.mainScreen().bounds
 public enum ServerUrl {
   case Local
   case Staging
+  case Production
   case Default
   public func getValue() -> String {
     switch self {
     case .Local: return "http://localhost:1337"
     case .Staging: return "https://drewslist-staging.herokuapp.com"
+    case .Production: return "https://drewslist-production.herokuapp.com"
 //    case .Default: return "http://localhost:1337"
-    case .Default: return "https://drewslist-staging.herokuapp.com"
+//    case .Default: return "https://drewslist-staging.herokuapp.com"
+    case .Default: return "https://drewslist-production.herokuapp.com"
     }
   }
 }
