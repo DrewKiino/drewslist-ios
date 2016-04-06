300513b44cf3175215486290a90311ea2516d075
Andrew Aquino <andrewaquino118@yahoo.com>
2016-04-06 08:44:51 -0700

    dbg

---
+++ DrewsList/Assets/View/IAPView.swift
@@ -0,0 +1,24 @@
+//
+//  IAPView.swift
+//  DrewsList
+//
+//  Created by Andrew Aquino on 4/6/16.
+//  Copyright © 2016 Totem. All rights reserved.
+//
+
+import Foundation
+import StoreKit
+
+public class IAPView: UIViewController {
+  
+  public static let prefix: String = "com.totemv.drewslistios."
+  private static let listingFee: String = prefix + "DL_ListingFee"
+  
+  
+  public override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    
+    
+  }
+}
